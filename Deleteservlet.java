48c74a98f9265af906181c80bb5e72f5c825710c
minal-ghatge <141444013+minal-ghatge@users.noreply.github.com>
2023-10-04 19:38:26 +0530

    Add files via upload

---
+++ Deleteservlet.java
@@ -0,0 +1,42 @@
+package com.webapp;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/Deleteservlet")
+public class Deleteservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Deleteservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		  
+		
+		
+		        String sid=request.getParameter("id");  
+		        int id=Integer.parseInt(sid);  
+		        EmpDao.delete(id);  
+		        response.sendRedirect("Viewservlet");  
+		    }  
+		
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String sid=request.getParameter("id");  
+	        int id=Integer.parseInt(sid);  
+	        EmpDao.delete(id);  
+	        response.sendRedirect("Viewservlet"); 
+	}
+
+}
---
+++ SaveServlet.java
@@ -0,0 +1,82 @@
+package com.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SaveServlet")
+public class SaveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public SaveServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//("<%@ page contentType="text/html; charset=UTF-8" %>");
+		  response.setContentType("text/html");  
+		 PrintWriter out=response.getWriter();  
+		 String name=request.getParameter("name");  
+        String password=request.getParameter("password");  
+        String email=request.getParameter("email");  
+        String country=request.getParameter("country");
+        Emp e=new Emp();  
+        e.setName(name);  
+        e.setPassword(password);  
+        e.setEmail(email);  
+        e.setCountry(country);  
+          
+        int status=EmpDao.save(e);  
+        if(status>0){  
+            out.print("<p>Record saved successfully!</p>");  
+            request.getRequestDispatcher("Index.jsp").include(request, response);  
+        }else{  
+            out.println("Sorry! unable to save record");  
+        }  
+          
+        out.close();  
+    }  
+  
+
+	
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//("<%@ page contentType='text/html; charset=UTF-8' %>"); 
+		  response.setContentType("text/html");  
+		PrintWriter out=response.getWriter(); 
+		String sid=request.getParameter("ID"); 
+		int id=Integer.parseInt(sid); 
+		String name=request.getParameter("name");  
+        String password=request.getParameter("password");  
+        String email=request.getParameter("email");  
+        String country=request.getParameter("country");
+        Emp e=new Emp();
+        e.setId(id);
+        e.setName(name);  
+        e.setPassword(password);  
+        e.setEmail(email);  
+        e.setCountry(country);  
+          
+        int status=EmpDao.save(e);  
+        if(status>0){  
+            out.print("<p>Record saved successfully!</p>");  
+            request.getRequestDispatcher("Index.jsp").include(request, response);  
+        }else{ 
+        	
+            out.println("Sorry! unable to save record");  
+        }  
+          
+        out.close();  
+    }  
+  
+
+	}
+
